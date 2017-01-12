c2a9cc6af15bba67a951f1a9f149f16f34ebd067
Chrs2324 <Williamsonchris111@gmail.com>
2017-01-12 16:05:15 -0600

    Add files via upload

--- ListTest.java
+++ ListTest.java
@@ -11,14 +11,19 @@ public class ListTest
     public static void main(String[] args)
     {
 	IntList myList = new IntList(10);
-	SortedIntList myList1 = new SortedIntList(10);
 	myList.add(100);
 	myList.add(50);
 	myList.add(200);
 	myList.add(25);
 	System.out.println("*** Unsorted List ***");
 	System.out.println(myList);
+
+	IntList myList2 = new SortedIntList(10);
+	myList2.add(100);
+	myList2.add(50);
+	myList2.add(200);
+	myList2.add(25);
 	System.out.println("*** Sorted List ***");
-	System.out.println(myList1.sortList());
+	System.out.println(myList2);
 	}
 }
